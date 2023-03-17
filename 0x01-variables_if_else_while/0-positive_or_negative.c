b4576bfbc9411485ca7c9afc0e175ef0faeadccc
Ayoub EL AKKAL <akkalayoub3@gmail.com>
2023-03-17 16:52:24 -0700

    new c negative or positive task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,27 @@
-readme file 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main()
+{
+
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+    if ( n > 0){
+        printf("%d is positive", n);
+    }
+    	/* your code goes there */
+    else if ( n == 0){
+        printf("%d is zero", n);
+    }else {
+        printf("%d is negative", n);
+    }
+
+    return 0;
+}
